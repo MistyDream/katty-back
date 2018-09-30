e418fff022b04547b38fe67c293c76c6bd5e7593
Remy Chauvin <remy.chauvin@epitech.eu>
2018-09-30 15:44:31 +0200

    First model: User
    - Migration

---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      firstname: DataTypes.STRING,
+      lastname: DataTypes.STRING,
+      email: DataTypes.STRING,
+      username: DataTypes.STRING,
+      peoples_meet: DataTypes.INTEGER,
+    },
+    {},
+  );
+  return User;
+};
