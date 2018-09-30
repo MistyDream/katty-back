da6cea83a9ea96f32936a5cb37a7002e3cb5f76f
Remy Chauvin <remy.chauvin@epitech.eu>
2018-09-30 15:45:03 +0200

    WIP : Authentication system
    - JWT tokens

--- src/server.js
+++ src/server.js
@@ -1,19 +1,35 @@
 const Hapi = require('hapi');
 
 const plugins = require('./plugins');
-const routes = require('./routes').default;
+const routes = require('./routes');
 // Read configuration file
-const config = require('./config/dev.json');
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/general.js')[env];
+
+// TODO: Validation function
+const validate = async (decoded, request) => {
+  if (!people[decoded.id]) {
+    return { isValid: false };
+  }
+  return { isValid: true };
+};
 
 exports.deployment = async () => {
   // Server configuration
   const server = Hapi.server({
     port: config.server_port,
-    host: config.server_hostname,
+    host: 'localhost',
   });
 
   // register plugins
   await server.register(plugins);
+
+  // Authentication mode
+  server.auth.strategy('jwt', 'jwt', {
+    key: config.jwt_secret,
+    validate,
+    verifyOptions: { algorithms: ['HS256'] }, // pick a strong algorithm
+  });
 
   // add routes
   server.route(routes);
